091f4077ebadd5c14d52633d8cb5313a716808c2
Delimaci <92929485+Delimaci@users.noreply.github.com>
2024-10-31 22:51:33 +0000

    lesson2 calculator

--- Lesson1/hello.py
+++ Lesson1/hello.py
@@ -1,7 +1,8 @@
 #31/10/24 - Python Lesson 1 (Harvard Course)
   # Ask the user for their name!
+name = input("What's your name? ").strip().title()
 
+#Split user's name into first name and last name
+first, last = name.split(" ")
 
-name = input("What's your name? ")
-print("I have awakened... ")
-print(name)
+print(f"I have awakened, {first}")
