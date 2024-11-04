2bec72c57779fef158020df2ba196d86fbbb6772
Delimaci <92929485+Delimaci@users.noreply.github.com>
2024-11-04 13:02:03 +0000

    Update mario.py

--- mario.py
+++ mario.py
@@ -8,12 +8,6 @@ def print_square(size):
     
     #for each row in square
     for i in range(size):
-        
-        #for each break in row
-        for j in range(size):
-            
-            # print brick
-            print("#", end="")
-        print()
+        print("#" * size)
         
 main()
